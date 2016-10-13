5e961cd4a924b9c877b2763c49e630c10351b6d7
wnortman@spireon.com <wnortman@spireon.com>
2016-10-13 13:45:16 -0700

    Adding Output of Repos Processed

--- bash-scripts/buildSourceTree.sh
+++ bash-scripts/buildSourceTree.sh
@@ -66,7 +66,7 @@ read GoDoIt
 # Create Subdirectory if it doesn't exist, user just ignore error if it exists
 mkdir $SubD
 
-curl --user "$Username:$Password1" -s "https://api.github.com/orgs/$GitHubOrg/repos?per_page=400"
+curl --user "$Username:$Password1" -s "https://api.github.com/orgs/$GitHubOrg/repos?per_page=400" | grep -e 'git_url*' | cut -d \" -f 4 
 
 # Connect to GitHub API and Extact up to 400 Repos
 curl --user "$Username:$Password1" -s "https://api.github.com/orgs/$GitHubOrg/repos?per_page=400" | grep -e 'git_url*' | cut -d \" -f 4 | cut -d / -f 5 | cut -d . -f 1 | awk -v subdir=$SubD -v org=$GitHubOrg '{print "/usr/bin/git clone git@github.com/" org "/"  $1 ".git"}' >  $SubD/cloneRepo.sh
